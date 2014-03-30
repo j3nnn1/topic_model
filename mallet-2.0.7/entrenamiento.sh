803114b8dea304d121c123372647c2656495a02f
j3nnn1 <jcmm986@gmail.com>
2014-03-30 16:42:29 -0300

    cleanning

--- mallet-2.0.7/entrenamiento.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for i in {4..28..2}; do
-	for y in {5..20..5}; do
-	sh bin/mallet train-topics --input noticias.mallet --optimize-interval $y --num-topics $i --word-topic-counts-file  noticias_word-topic-counts.$i.$y.txt --output-state noticias-topic-state.$i.$y.gz --output-topic-keys noticias_keys.$i.$y.txt --output-doc-topics noticias_composition.$i.$y.txt --inferencer-filename noticias-inferencer.$i.$y.model --evaluator-filename noticias-evaluator.$i.$y.evaluator
-	done
-done
--- mallet-2.0.7/inferencer4.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-i=4
-for y in {5..20..5}; do
-	sh bin/mallet infer-topics --inferencer noticias-inferencer.$i.$y.model --input noticias_test.mallet --output-doc-topics noticias-inferencer.$i.$y.model.output  --num-iterations 10 --sample-interval 4
-done
